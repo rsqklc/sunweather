0ecabea457464d2b2c6fcc5748013ff28a090613
Tony <tony@gmail.com>
2017-03-02 12:22:04 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/sunweater/android/WeatherActivity.java
+++ app/src/main/java/com/sunweater/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.sunweater.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -208,5 +209,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startActivity(intent);
     }
 }
